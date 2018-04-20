4887de20b649871a34ac5ffa61bef1b8f9e189d1
lordkangz <lordkangz@gmail.com>
2018-04-20 16:37:08 -0400

    Invoices Aggregation binding (Step 20).

--- component.js
+++ component.js
@@ -1,30 +1,38 @@
 sap.ui.define([
-   "sap/ui/core/UIComponent",
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
-   "use strict";
-   return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-            metadata : {
-			manifest: "json"
-	},
-      init : function () {
-         // call the init function of the parent
-         UIComponent.prototype.init.apply(this, arguments);
-         // set data model
-         var oData = {
-            recipient : {
-               name : "UI5"
-            }
-         };
-         var oModel = new JSONModel(oData);
-         this.setModel(oModel);
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/demo/walkthrough/controller/HelloDialog"
 
-         // set i18n model
-         var i18nModel = new ResourceModel({
-            bundleName : "sap.ui.demo.walkthrough.i18n.i18n"
-         });
-         this.setModel(i18nModel, "i18n");
-      }
-   });
+], function (UIComponent, JSONModel, HelloDialog) {
+	"use strict";
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+		metadata : {
+			manifest : "json"
+		},
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+			// set data model
+			var oData = {
+				recipient : {
+					name : "UI5"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			// set dialog
+			this._helloDialog = new HelloDialog(this.getRootControl());
+		},
+
+
+		exit : function() {
+			this._helloDialog.destroy();
+			delete this._helloDialog;
+		},
+
+		openHelloDialog : function () {
+			this._helloDialog.open();
+		}
+	});
 });
